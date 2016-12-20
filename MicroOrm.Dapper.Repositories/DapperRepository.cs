ee977f096597cb0ad19cfe94413c560193aaf314
David Markusfeld <david@markusfeld.at>
2016-12-20 10:03:45 +0100

    another fix

--- MicroOrm.Dapper.Repositories/DapperRepository.cs
+++ MicroOrm.Dapper.Repositories/DapperRepository.cs
@@ -82,7 +82,7 @@ namespace MicroOrm.Dapper.Repositories
         public virtual TEntity Find<TChild1>(Expression<Func<TEntity, object>> tChild1, Expression<Func<TEntity, bool>> predicate, IDbTransaction transaction = null)
         {
             var queryResult = SqlGenerator.GetSelectFirst(predicate, tChild1);
-            return FindAll<TChild1>(queryResult, tChild1, tran).FirstOrDefault();
+            return FindAll<TChild1>(queryResult, tChild1, transaction).FirstOrDefault();
         }
         
         /// <summary>
